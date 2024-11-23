25f224a1bb18c9e8a73448befc99d274365f0e0c
piygr <grover1piyush@gmail.com>
2024-11-23 16:40:01 +0530

    updated model

--- models/model.py
+++ models/model.py
@@ -1,21 +1,27 @@
+import torch
 import torch.nn as nn
 
 class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3)
-        self.fc1 = nn.Linear(32 * 5 * 5, 64)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3)
+        self.bn1 = nn.BatchNorm2d(8)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.fc1 = nn.Linear(16, 64)
+        self.dropout = nn.Dropout(0.1)
         self.fc2 = nn.Linear(64, 10)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.dropout(x)
         x = self.pool(x)
-        x = self.relu(self.conv2(x))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.dropout(x)
         x = self.pool(x)
-        x = x.view(-1, 32 * 5 * 5)  # Flatten the tensor
+        x = x.view(-1, 64)  # Flatten the tensor
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
